28284a017c13723d145af1e78fbea7a9d219eab2
Christian Kjær <c-kj@mail.dk>
2021-04-23 13:27:17 +0200

    Fix: InsertionSort

--- src/Algorithms/InsertionSort.java
+++ src/Algorithms/InsertionSort.java
@@ -1,5 +1,6 @@
 package Algorithms;
 
+import Resourses.Settings;
 import Resourses.arrayPanel;
 
 import java.awt.*;
@@ -13,12 +14,23 @@ public class InsertionSort extends SortingAlgorithm {
     public void run() {
         int n = arr.dataBars.size();
         for (int i = 1; i < n; i++) {
-            for (int j = i; j > 0 && arr.dataBars.get(j).getHeight() < arr.dataBars.get(j - 1).getHeight(); j--) {
-                arr.swap(j, j - 1);
-                arr.setColor(j, Color.BLACK);
-                arr.setColor(j - 1, Color.BLACK);
+            for (int j = i; j > 0; j--) {
+                arr.setColor(i, Color.YELLOW);
+                arr.compare(j, j - 1);
+                arr.setColor(i, Color.YELLOW);
+                if (arr.dataBars.get(j).getHeight() < arr.dataBars.get(j - 1).getHeight()) {
+                    arr.swap(j, j - 1);
+                    arr.setColor(i, Color.YELLOW);
+                    arr.setColor(j, Color.BLACK);
+                    arr.setColor(j - 1, Color.BLACK);
+                } else {
+                    arr.setColor(j, Color.BLACK);
+                    arr.setColor(j - 1, Color.BLACK);
+                    break;
+                }
             }
         }
+        arr.setColor(n - 1, Color.BLACK);
         arr.done();
     }
 
